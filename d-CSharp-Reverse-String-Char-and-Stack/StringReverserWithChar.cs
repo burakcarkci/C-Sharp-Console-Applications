5a84622adc79755c39ef9aa6cf0b3220bbe4967d
Burak <burakcarkci@hotmail.com>
2020-01-30 15:12:00 -0500

    Reverse String

---
+++ d-CSharp-Reverse-String-Char-and-Stack/StringReverserWithChar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class StringReverserWithChar
+    {
+        public string CharReverse(string item)
+        {
+            char[] charArray = item.ToCharArray();
+            string itemReversed = "";
+
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                itemReversed = itemReversed + charArray[i];
+            }
+            return itemReversed;
+        }
+    }
+}
